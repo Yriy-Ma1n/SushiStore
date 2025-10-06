9a43fe7e7df9d2e0210295984477ca146c6fe740
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-10-06 22:15:19 +0300

    create CatalogProductCard component

---
+++ src/Catalog/CatalogProductCard/CatalogProductCard.tsx
@@ -0,0 +1,9 @@
+import "./CatalogProductCard.css";
+
+export function CatalogProductCard(){
+    return(<div className="productCardBlock">
+        <img src="#" alt="Product image" />
+        <p className="title"></p>
+        <p className="subtitle"></p>
+    </div>)
+}
