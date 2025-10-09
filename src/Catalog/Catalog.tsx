48935bb6fde23fc1ee96ca4cacca2fef37d0c662
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-10-09 11:47:53 +0300

    add stiles to product zone

--- src/Catalog/Catalog.tsx
+++ src/Catalog/Catalog.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./Catalog.css";
 import { CatalogProductCard } from "./CatalogProductCard/CatalogProductCard";
 import { FilterCatalog } from "./FilterCatalog/FilterCatalog";
+import type { Product } from "../Types/Product";
 
 
 export function Catalog(){
+    const [products, setProducts] = useState<Product[]>([]);
+
+    async function requestCatalogProducts(){
+        try{
+            const res = await fetch("http://localhost:3000/products",{method:"GET"});
+            if(!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+            const data = await res.json();
+            setProducts(data);
+            console.log(data);
+        }
+        catch(err){
+            console.error(`Fetch reqest has problems:`, err);
+        }
+    }
+
+    useEffect(()=>{
+        requestCatalogProducts();
+    }, []);
 
 
     return (<div className="CatalogSection">
@@ -11,8 +31,8 @@ export function Catalog(){
             <h1>Sushi</h1>
             <FilterCatalog/>
         </div>
-        <div className="ProductZone">
-            <CatalogProductCard/>
+        <div className="productZone">
+            {products.map(item => <div className="item" key={item.id}> <CatalogProductCard {...item}/> </div>)}
         </div>
     </div>);
 }
--- src/Catalog/CatalogProductCard/CatalogProductCard.tsx
+++ src/Catalog/CatalogProductCard/CatalogProductCard.tsx
@@ -1,11 +1,14 @@
+import type { Product } from "../../Types/Product";
 import "./CatalogProductCard.css";
 
-export function CatalogProductCard(){
+export function CatalogProductCard(props:Product){
+
+    
     return(<div className="productCardBlock">
-        <img src="#" alt="Product image" />
+        <img src={props.img} alt="Product image" />
         <div className="information">
-            <p className="title">solid</p>
-            <p className="subtitle">solid</p>
+            <p className="title">{props.name}</p>
+            <p className="subtitle">{props.description.slice(0,60)}...</p>
         </div>
     </div>)
 }
