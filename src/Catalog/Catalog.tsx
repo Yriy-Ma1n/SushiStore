15ffa4966c3010c95ed1e94952fc5f12361cf298
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-10-10 20:01:34 +0300

    complete pagination

--- src/Catalog/Catalog.tsx
+++ src/Catalog/Catalog.tsx
@@ -7,6 +7,11 @@ import type { Product } from "../Types/Product";
 
 export function Catalog(){
     const [products, setProducts] = useState<Product[]>([]);
+    let [renderProducts, setRenderProducts] = useState<Product[]>([]);
+    const [page, setPage] = useState(1);
+    const limit:number = 12;
+    const total:number = products.length;
+
 
     async function requestCatalogProducts(){
         try{
@@ -14,6 +19,7 @@ export function Catalog(){
             if(!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
             const data = await res.json();
             setProducts(data);
+            setRenderProducts(data.slice(0, limit));
             console.log(data);
         }
         catch(err){
@@ -21,9 +27,20 @@ export function Catalog(){
         }
     }
 
+
     useEffect(()=>{
         requestCatalogProducts();
     }, []);
+
+
+   function changePage() {
+    setRenderProducts(products.slice(limit * page - limit, limit * page));
+    }
+
+    useEffect(() => {
+        changePage();
+    }, [page, products]);
+
 
 
     return (<div className="CatalogSection">
@@ -32,7 +49,30 @@ export function Catalog(){
             <FilterCatalog/>
         </div>
         <div className="productZone">
-            {products.map(item => <div className="item" key={item.id}> <CatalogProductCard {...item}/> </div>)}
+            {renderProducts.map(item => <div className="item" key={item.id}> <CatalogProductCard {...item}/> </div>)}
+        </div>
+
+        <div className="catalogPagination">
+            <button
+             disabled={page === 1}
+             onClick={()=> {setPage((current)=> current - 1)}}
+             className="arrowLeft"
+             >Prev</button>
+            {
+                Array.from({ length: Math.ceil(total / limit) }, (_, i)=>(
+                    <button
+                     onClick={()=> setPage(i + 1)}
+                     className={page === i + 1 ? "active" : "disable"}
+                    >
+                        {i + 1}
+                    </button>
+                ))
+            }
+            <button
+             disabled={page === Math.ceil(total / limit)}
+             onClick={()=> setPage((current)=> current + 1)}
+             className="arrowRight"
+             >Next</button>
         </div>
     </div>);
 }
