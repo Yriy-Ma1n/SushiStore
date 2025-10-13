4228e6b39cf8ee404100e177af6c5eafb9246d6b
Ma1n <kskiller2020@gmail.com>
2025-10-13 16:11:19 +0200

    added test to work check render navigation

---
+++ src/components/Recomendation/PaginationCount/Pagination.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PaginationMainPage from "./Pagination";
+import { expect, test } from "vitest"
+import { render, screen } from "@testing-library/react"
+import type { Product } from "../../../Types/Product";
+
+
+
+test("it checks render navigation", () => {
+    const array:Product[] = Array.from({ length: 9 })
+    
+    const { getByText } = render(
+        <PaginationMainPage backupArr={array} setPage={(i: number) => i} />
+    )
+
+    const text = getByText('2')
+    expect(text).toBeTruthy()
+})
