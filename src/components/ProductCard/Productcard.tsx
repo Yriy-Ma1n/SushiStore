47c40ec287a4e346d54b38357400ca62517a3ee9
Ma1n <kskiller2020@gmail.com>
2025-10-22 00:05:17 +0200

    added related items

--- src/components/ProductCard/Productcard.tsx
+++ src/components/ProductCard/Productcard.tsx
@@ -3,10 +3,13 @@ import { useParams } from "react-router-dom"
 import type { Product } from "../../Types/Product"
 import styles from "./Productcart.module.css"
 import ProductAttribute from "./productAttribute/productAttribute"
+import { RelatedItems } from "./RelatedItems/RelatedItems"
+
 
 export default function ProductCart() {
     const { id } = useParams()
     const [product, setProduct] = useState<Product | null>(null)
+    
     useEffect(() => {
         fetch(`http://localhost:3000/products/${id}`)
             .then(res => res.json())
@@ -15,6 +18,8 @@ export default function ProductCart() {
                 console.log(data[0])
             })
     }, [])
+
+
     if (product && Object.keys(product).length === 0) {
         return <div>Tovar not found</div>
     } else {
@@ -28,16 +33,6 @@ export default function ProductCart() {
                     <p className={styles.description}>{product?.description}</p>
                     <div className={styles.largeDesc}>
                         <ProductAttribute attribute={product?.products_attributes!}/>
-                        {/* {product?.products_attributes.map(item => {
-                            return <div key={item.attribute_id} className={styles.containerDesc}>
-                                <p className={styles.leftDesc}>
-                                    {item.name_of_attribute}
-                                </p>
-                                <p className={styles.rightDesc}>
-                                    {item.value} {item.value.length === 2 ? 'Шт' : 'Грам'}
-                                </p>
-                            </div>
-                        })} */}
                     </div>
                     <div className={styles.costBuy}>
                         <h1>{product?.price} Грн</h1>
@@ -50,7 +45,9 @@ export default function ProductCart() {
                     </div>
                 </div>
             </div>
-            <div className={styles.lowerContent}></div>
+            <div className={styles.lowerContent}>
+                <RelatedItems/>
+            </div>
         </div>
     }
 
