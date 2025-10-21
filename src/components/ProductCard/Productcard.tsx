c727ebe2c63e4592a0cbc69ee539044eb3821bce
Ma1n <kskiller2020@gmail.com>
2025-10-21 11:42:14 +0200

    added ability to open product page

--- src/components/ProductCard/Productcard.tsx
+++ src/components/ProductCard/Productcard.tsx
@@ -1,15 +1,56 @@
 import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
-import type { RootState } from "../../store"
+import type { Product } from "../../Types/Product"
+import styles from "./Productcart.module.css"
 
-export default function ProductCart(){
+export default function ProductCart() {
     const { id } = useParams()
-    const data = useSelector((state: RootState) => state.product)
-    useEffect(()=>{
-        console.log(data)
-    },[])
+    const [product, setProduct] = useState<Product | null>(null)
+    useEffect(() => {
+        fetch(`http://localhost:3000/products/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setProduct(data[0])
+                console.log(data[0])
+            })
+    }, [])
+    if (product && Object.keys(product).length === 0) {
+        return <div>Tovar not found</div>
+    } else {
+        return <div className={styles.container}>
+            <div className={styles.upperContent}>
+                <div className={styles.imageSide}>
+                    <img src={product?.img} alt="Tovar Image" />
+                </div>
+                <div className={styles.contentSide}>
+                    <h1 className={styles.name}>{product?.name}</h1>
+                    <p className={styles.description}>{product?.description}</p>
+                    <div className={styles.largeDesc}>
+                        {product?.products_attributes.map(item => {
+                            return <div key={item.attribute_id} className={styles.containerDesc}>
+                                <p className={styles.leftDesc}>
+                                    {item.name_of_attribute}
+                                </p>
+                                <p className={styles.rightDesc}>
+                                    {item.value} {item.value.length === 2 ? 'Шт' : 'Грам'}
+                                </p>
+                            </div>
+                        })}
+                    </div>
+                    <div className={styles.costBuy}>
+                        <h1>{product?.price} Грн</h1>
+                        <button className={styles.buy}>
+                            <span className="material-symbols-outlined">
+                                shopping_cart_checkout
+                            </span>
+                            Add to Cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.lowerContent}></div>
+        </div>
+    }
 
 
-    return<div>{id}</div>
 }  
