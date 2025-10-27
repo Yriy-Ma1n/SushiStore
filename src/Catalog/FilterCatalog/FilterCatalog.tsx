cda42c8c93c0f679be19000b8ed9d48d345b0ee6
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-10-27 19:37:41 +0200

    refactoring filter catalog

--- src/Catalog/FilterCatalog/FilterCatalog.tsx
+++ src/Catalog/FilterCatalog/FilterCatalog.tsx
@@ -11,9 +11,16 @@ export function FilterCatalog() {
   const [displayNewDeals, setDisplayNewDeals] = useState("none");
 
   // для кастомного чекбокса
-  const [range1, setRange1] = useState("checkBoxItemFalse");
-  const [range2, setRange2] = useState("checkBoxItemFalse");
-  const [range3, setRange3] = useState("checkBoxItemFalse");
+  const [range1, setRange1] = useState("checkBoxItemTrue");
+  const [range2, setRange2] = useState("checkBoxItemTrue");
+  const [range3, setRange3] = useState("checkBoxItemTrue");
+  function setCheckBox(range: string, setRange: Function) {
+    if (range === "checkBoxItemFalse") {
+      setRange("checkBoxItemTrue");
+    } else {
+      setRange("checkBoxItemFalse");
+    }
+  }
 
   return (
     <div className="filter">
@@ -24,20 +31,7 @@ export function FilterCatalog() {
         >
           <span className="filterElement">
             <p>Price</p>
-            <svg
-              enableBackground="new 0 0 50 50"
-              height="50px"
-              id="Layer_1"
-              version="1.1"
-              viewBox="0 0 50 50"
-              width="50px"
-              xmlSpace="preserve"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-            >
-              <rect fill="none" height="50" width="50" />
-              <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-            </svg>
+            <span className="material-symbols-outlined">keyboard_arrow_down</span>
           </span>
           <div className="fakeSpace"></div>
           <div style={{ display: displayPrice }} className="drop-downElement">
@@ -48,45 +42,21 @@ export function FilterCatalog() {
               </div>
 
               <div className="CheckBox">
-                <div
-                  onClick={() => {
-                    if (range1 === "checkBoxItemFalse") {
-                      setRange1("checkBoxItemTrue");
-                    } else {
-                      setRange1("checkBoxItemFalse");
-                    }
-                  }}
-                >
+                <div onClick={() => setCheckBox(range1, setRange1)}>
                   <div className={range1} id="range1">
                     <span className="material-symbols-outlined">check</span>
                   </div>
                   <label htmlFor="range1">300-900</label>
                 </div>
 
-                <div
-                  onClick={() => {
-                    if (range2 === "checkBoxItemFalse") {
-                      setRange2("checkBoxItemTrue");
-                    } else {
-                      setRange2("checkBoxItemFalse");
-                    }
-                  }}
-                >
+                <div onClick={() => setCheckBox(range2, setRange2)}>
                   <div className={range2} id="range2">
                     <span className="material-symbols-outlined">check</span>
                   </div>
                   <label htmlFor="range2">900-1500</label>
                 </div>
 
-                <div
-                  onClick={() => {
-                    if (range3 === "checkBoxItemFalse") {
-                      setRange3("checkBoxItemTrue");
-                    } else {
-                      setRange3("checkBoxItemFalse");
-                    }
-                  }}
-                >
+                <div onClick={() => setCheckBox(range3, setRange3)}>
                   <div className={range3} id="range3">
                     <span className="material-symbols-outlined">check</span>
                   </div>
@@ -105,26 +75,10 @@ export function FilterCatalog() {
         >
           <span className="filterElement">
             <p>Category</p>
-            <svg
-              enableBackground="new 0 0 50 50"
-              height="50px"
-              id="Layer_1"
-              version="1.1"
-              viewBox="0 0 50 50"
-              width="50px"
-              xmlSpace="preserve"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-            >
-              <rect fill="none" height="50" width="50" />
-              <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-            </svg>
+            <span className="material-symbols-outlined">keyboard_arrow_down</span>
           </span>
           <div className="fakeSpace"></div>
-          <div
-            style={{ display: displayCategory }}
-            className="drop-downElement"
-          >
+          <div style={{ display: displayCategory }} className="drop-downElement">
             <button className="drop-downElementAccept">Accept</button>
           </div>
         </li>
@@ -135,20 +89,7 @@ export function FilterCatalog() {
         >
           <span className="filterElement">
             <p>Filling</p>
-            <svg
-              enableBackground="new 0 0 50 50"
-              height="50px"
-              id="Layer_1"
-              version="1.1"
-              viewBox="0 0 50 50"
-              width="50px"
-              xmlSpace="preserve"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-            >
-              <rect fill="none" height="50" width="50" />
-              <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-            </svg>
+            <span className="material-symbols-outlined">keyboard_arrow_down</span>
           </span>
           <div className="fakeSpace"></div>
           <div style={{ display: displayFilling }} className="drop-downElement">
@@ -162,20 +103,7 @@ export function FilterCatalog() {
         >
           <span className="filterElement">
             <p>Weight</p>
-            <svg
-              enableBackground="new 0 0 50 50"
-              height="50px"
-              id="Layer_1"
-              version="1.1"
-              viewBox="0 0 50 50"
-              width="50px"
-              xmlSpace="preserve"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-            >
-              <rect fill="none" height="50" width="50" />
-              <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-            </svg>
+            <span className="material-symbols-outlined">keyboard_arrow_down</span>
           </span>
           <div className="fakeSpace"></div>
           <div style={{ display: displayWeight }} className="drop-downElement">
@@ -189,26 +117,10 @@ export function FilterCatalog() {
         >
           <span className="OrangeFilterElement">
             <p>New & Deals</p>
-            <svg
-              enableBackground="new 0 0 50 50"
-              height="50px"
-              id="Layer_1"
-              version="1.1"
-              viewBox="0 0 50 50"
-              width="50px"
-              xmlSpace="preserve"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-            >
-              <rect fill="none" height="50" width="50" />
-              <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-            </svg>
+            <span className="material-symbols-outlined">keyboard_arrow_down</span>
           </span>
           <div className="fakeSpace"></div>
-          <div
-            style={{ display: displayNewDeals }}
-            className="drop-downElementYellow"
-          >
+          <div style={{ display: displayNewDeals }} className="drop-downElementYellow">
             <button className="drop-downElementAccept">Accept</button>
           </div>
         </li>
