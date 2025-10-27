6a40999fa116123a72698574d6474c10252baac1
Ma1n <kskiller2020@gmail.com>
2025-10-27 13:54:10 +0100

    added Cart Tile and some functional

--- src/components/Cart/CartTile/CartTile.tsx
+++ src/components/Cart/CartTile/CartTile.tsx
@@ -1,11 +1,25 @@
 import { useState } from "react"
 import styles from "./CartTile.module.css"
+import Count from "./Count/Count"
 
-export default function CartTile(props: { src: string, name: string, cost: string, amount: number, allamount:number }) {
-    const [cost, setConst] = useState(props.cost)
+export default function CartTile(props: { src: string, name: string, cost: string, amount: number, allamount: number }) {
+    const [cost, setCost] = useState(props.cost)
     const [count, setCount] = useState(props.amount)
+    const [defaultCost] = useState(props.cost)
 
-    return <div className={styles.TileContainer}> {/*flex space-between*/}
+    const plusButton = () => {
+        setCount(curr => curr + 1)
+        setCost((curr) => curr = String(+defaultCost + +cost) + '.00')
+    }
+
+    const minusButton = () => {
+        if(count === 1) return
+        setCount(curr => curr - 1)
+        setCost((curr) => curr = String(+curr - +defaultCost) + '.00')
+       
+    }
+
+    return <div className={styles.TileContainer}>
         <div className={styles.leftSide}>
             <img src={props.src} alt="img" />
             <div className={styles.info}>
@@ -15,23 +29,7 @@ export default function CartTile(props: { src: string, name: string, cost: strin
             </div>
         </div>
         <div className={styles.rightSide}>
-            <div className={styles.buttonsCount}>
-
-                <button className={styles.addButton}>
-                    <span className="material-symbols-outlined">
-                        add
-                    </span>
-                </button>
-
-                <p>{count}</p>
-
-                <button className={styles.minusButton}>
-                    <span className="material-symbols-outlined">
-                        check_indeterminate_small
-                    </span>
-                </button>
-
-            </div>
+            <Count count={count} plusBtn={plusButton} minusBtn={minusButton} />
         </div>
     </div>
 }
